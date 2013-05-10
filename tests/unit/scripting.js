c2ddc5a65faa350aa0df23a8d195561b129b5ff8
Rajeeva <acharya.rajiv@gmail.com>
2013-05-10 12:36:26 +0530

    Code review changes for Redis 2.6
    Code change after review

--- tests/unit/scripting.js
+++ tests/unit/scripting.js
@@ -379,12 +379,9 @@ exports.Scripting = (function () {
 			var test_case = 'EVAL - Script can\'t run more than configured time limit';
 			client.config('set', 'lua-time-limit', 1, function (err, res) {
 
-				client.eval('local i = 0 while false do i=i+1 end', 0, function (err, res) {
-					if (err) {
-						errorCallback(err);
-					}
+				client.eval('local i = 0 while true do i=i+1 end', 0, function (err, res) {
 					try {
-						if (!assert.equal(err, 'this is DB 0', test_case))
+						if (!assert.equal(err, 'execution time', test_case))
 							ut.pass(test_case);
 					} catch (e) {
 						ut.fail(e, true);
